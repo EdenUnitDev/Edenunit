d497c645494f40c3ca758e4eae64992fd436becc
EdenUnitDev <mrskeltal777@gmail.com>
2017-10-05 16:57:34 -0400

    i call it bold and brash
    ehahah ah

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x33, 0xA1, 0x4F, 0x6L, 0xC4, 0xE4, 0xF3, 0xd2, 0x3D, 0x2d, 0x1f, 0xFL, 0x44, 0x2E, 0x1C, 0x99 } };
+}
