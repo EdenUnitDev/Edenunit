938478754a0dcd1ec874e3040f648690b2551c8b
Ivan Maxwell <32554488+EdenUnitDev@users.noreply.github.com>
2017-12-10 23:19:27 -0500

    Genesis block

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "Edenunit";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "010501ff0001aad188f103029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121010cba5483f6deeed62dba69ad8a96f22bc65cb4b78bd09fa45299958c01090429";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
