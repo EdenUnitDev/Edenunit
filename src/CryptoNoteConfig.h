ba0765264c94ca3977dbcc8a562c67e58f5f6e7c
Ivan Maxwell <32554488+EdenUnitDev@users.noreply.github.com>
2017-12-11 16:06:11 -0500

    Seed node changes

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"insert.seed.node.here:8080",
-  //"insert.seed.node.here:8080",
+  //"35.226.137.84:8080",
+  //"35.188.44.219:8080",
 };
 
 struct CheckpointData {
